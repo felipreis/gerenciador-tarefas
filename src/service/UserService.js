9870162d2a2561b5da02f66fd74c5fc090e4b00d
felipreis <felipreisdaniel@gmail.com>
2026-06-26 16:25:57 -0300

    fase 3 part 1 : add bycrypt on db

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,4 +1,6 @@
 import UserRepository from '../repositoires/UserRepository.js'
+import bcrypt from 'bcryptjs';
+
 
 async function getAll(){
     return await UserRepository.getAll();
@@ -9,7 +11,9 @@ async function getById(id){
 }
 
 async function create(body){
-    return await UserRepository.create(body)
+    // não alterar o objeto que vem da requisição
+    const hashedBody = {...body, password: await bcrypt.hash(body.password,10) };
+    return await UserRepository.create(hashedBody)
 }
 
 
