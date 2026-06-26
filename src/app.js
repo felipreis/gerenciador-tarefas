53d0a385137a6244653c3fe319904c09888096a1
felipreis <felipreisdaniel@gmail.com>
2026-06-25 22:31:11 -0300

    fase 1: api online

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import router from './routes/routes.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {console.log(`Server online at port ${PORT}`)} )
+
+
