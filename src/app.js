9eaaa8c059933ba1c01034354ed33624168ca3f1
felipreis <felipreisdaniel@gmail.com>
2026-07-03 17:40:43 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ import Project from './model/Project.js'
 import Task from './model/Task.js';
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from './docs/swagger.js'
+import cors from 'cors'
 
 
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
